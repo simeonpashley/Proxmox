dbd1c085acdcdc83e3320498e8c6a9e66d3d53e0
Simeon Pashley <1629421+simeonpashley@users.noreply.github.com>
2022-06-16 12:20:03 +0100

    Update and rename ha-to-mysql.sh to docs/ha-to-mysql.sh

--- docs/ha-to-mysql.sh
+++ docs/ha-to-mysql.sh
@@ -6,7 +6,7 @@ then
 fi
 
 # 2022-06-16 : SED configured for migrating HomeAssistant data
-sed \
+zcat $1 | sed \
 -e '/PRAGMA.*;/ d' \
 -e '/BEGIN TRANSACTION.*/ d' \
 -e '/COMMIT;/ d' \
@@ -20,5 +20,4 @@ sed \
 -e 's/CREATE TABLE \(`\w\+`\)/DROP TABLE IF EXISTS \1;\nCREATE TABLE \1/' \
 -e 's/\(CREATE TABLE.*\)\(PRIMARY KEY\) \(AUTOINCREMENT\)\(.*\)\();\)/\1AUTO_INCREMENT\4, PRIMARY KEY(id)\5/' \
 -e "s/'t'/1/g" \
--e "s/'f'/0/g" \
-$1
+-e "s/'f'/0/g"
